e96a3f5abf2801edd3f3121a2476bc16d00930da
TimeTravelPenguins <TimeTravelPenguin@gmail.com>
2023-12-13 15:06:55 +1100

    Refactored and implemented `Code` variants

--- src/Printing.hs
+++ src/Printing.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedLists #-}
-
 module Printing (
   Message,
   URI,
   SGRStack,
+  getCurrentDefaults,
   startSGRScope,
+  startSGRScopeCode,
   nestedSGRScope,
+  nestedSGRScopeCode,
   sgrHyperlink,
+  sgrHyperlinkCode,
   sgrHyperlinkFile,
+  sgrHyperlinkFileCode,
   sgrGray,
   sgrYellow,
   sgrBlue,
@@ -17,6 +18,7 @@ module Printing (
   sgrPutStr,
   sgrPutStrLn,
   debug,
+  debugCode,
 ) where
 
 import Control.Monad.State.Strict (
@@ -28,8 +30,6 @@ import Control.Monad.State.Strict (
  )
 import Data.Colour.RGBSpace (RGB)
 import Data.Foldable (toList)
-import Data.Sequence (Seq, (><))
-import Data.Sequence qualified as Seq
 import Data.Word (Word16)
 import System.Console.ANSI (
   Color (Blue, Red, White, Yellow),
@@ -39,14 +39,16 @@ import System.Console.ANSI (
   SGR (Reset, SetColor, SetConsoleIntensity),
   hGetLayerColor,
   hyperlink,
+  hyperlinkCode,
   setSGR,
+  setSGRCode,
  )
 import System.IO (stdout)
 
 type Message = String
 type URI = String
 
-type SGRStack = StateT (Seq SGR) IO
+type SGRStack m a = StateT [SGR] m a
 
 sgrGray :: [SGR]
 sgrGray = [SetColor Foreground Dull White]
@@ -61,40 +63,59 @@ sgrRed :: [SGR]
 sgrRed = [SetColor Foreground Vivid Red]
 
 -- | Set the current graphics options using the current stack state
-setSGRScope :: SGRStack ()
+setSGRScope :: MonadIO m => SGRStack m ()
 setSGRScope = do
-  sgrs <- fmap toList get
+  sgrs <- toList <$> get
   liftIO $ setSGR [Reset]
   liftIO $ setSGR sgrs
 
+setSGRScopeCode :: Monad m => SGRStack m String
+setSGRScopeCode = do
+  sgrs <- toList <$> get
+  return $ setSGRCode [Reset] <> setSGRCode sgrs
+
 -- TODO: Use this and test
-getCurrentDefaults :: IO (Maybe [RGB Word16])
+getCurrentDefaults :: MonadIO m => m (Maybe [RGB Word16])
 getCurrentDefaults = do
-  mBC <- hGetLayerColor stdout Background
-  mFC <- hGetLayerColor stdout Foreground
+  mBC <- liftIO $ hGetLayerColor stdout Background
+  mFC <- liftIO $ hGetLayerColor stdout Foreground
   return $ sequence [mBC, mFC]
 
--- | Start a new SGR scope with the provided initial SGR paramaters.
-startSGRScope :: [SGR] -> SGRStack a -> IO a
+-- | Start a new SGR scope with the provided initial SGR parameters.
+startSGRScope :: MonadIO m => [SGR] -> SGRStack m a -> m a
 startSGRScope sgrs f = do
-  res <- evalStateT (setSGRScope >> f) (Seq.fromList sgrs)
-  setSGR [Reset]
+  res <- evalStateT (setSGRScope >> f) sgrs
+  liftIO $ setSGR [Reset]
   return res
+
+-- | Start a new SGR scope with the provided initial SGR parameters.
+startSGRScopeCode :: Monad m => [SGR] -> SGRStack m String -> m String
+startSGRScopeCode sgrs f = do
+  res <- evalStateT (setSGRScopeCode >> f) sgrs
+  return $ setSGRCode [Reset] <> res
 
 -- | Start an inner SGR scope, appending the provided options to the stack.
 -- These options are set in order, after previously provided options.
-nestedSGRScope :: [SGR] -> SGRStack a -> SGRStack a
+nestedSGRScope :: MonadIO m => [SGR] -> SGRStack m a -> SGRStack m a
 nestedSGRScope sgrs f = do
   prevSGR <- get
-  res <- withStateT (>< Seq.fromList sgrs) (setSGRScope >> f)
+  res <- withStateT (<> sgrs) (setSGRScope >> f)
   put prevSGR
   setSGRScope
   return res
 
-sgrPutStr :: Message -> SGRStack ()
+nestedSGRScopeCode :: Monad m => [SGR] -> SGRStack m String -> SGRStack m String
+nestedSGRScopeCode sgrs f = do
+  prevSGR <- get
+  res <- withStateT (<> sgrs) (setSGRScopeCode >> f)
+  put prevSGR
+  set <- setSGRScopeCode
+  return $ res <> set
+
+sgrPutStr :: MonadIO m => Message -> SGRStack m ()
 sgrPutStr = liftIO . putStr
 
-sgrPutStrLn :: Message -> SGRStack ()
+sgrPutStrLn :: MonadIO m => Message -> SGRStack m ()
 sgrPutStrLn msg = do
   prevSGR <- get
   sgrPutStr msg
@@ -103,14 +124,26 @@ sgrPutStrLn msg = do
   put prevSGR
   setSGRScope
 
-sgrHyperlink :: URI -> Message -> SGRStack ()
+sgrHyperlink :: MonadIO m => URI -> Message -> SGRStack m ()
 sgrHyperlink uri msg = do
   nestedSGRScope sgrBlue (liftIO $ hyperlink uri msg)
 
-sgrHyperlinkFile :: URI -> SGRStack ()
+sgrHyperlinkCode :: Monad m => String -> String -> SGRStack m String
+sgrHyperlinkCode uri msg = do
+  nestedSGRScopeCode sgrBlue (return $ hyperlinkCode uri msg)
+
+sgrHyperlinkFile :: MonadIO m => URI -> SGRStack m ()
 sgrHyperlinkFile uri = sgrHyperlink ("file://" <> uri) uri
 
-debug :: String -> IO ()
+sgrHyperlinkFileCode :: Monad m => String -> SGRStack m String
+sgrHyperlinkFileCode uri = sgrHyperlinkCode ("file://" <> uri) uri
+
+debug :: MonadIO m => String -> m ()
 debug msg = do
   startSGRScope [SetColor Foreground Vivid Red] $ do
     sgrPutStrLn $ unwords ["DEBUG:", msg]
+
+debugCode :: Monad m => String -> m String
+debugCode msg = do
+  startSGRScopeCode [SetColor Foreground Vivid Red] $ do
+    return $ unwords ["DEBUG:", msg]
